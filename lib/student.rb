a4a057edacb9ce6149d3d889ff127b840ca1aa10
Caridad Rivera <ccrive10@gmail.com>
2019-02-28 22:32:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,16 @@
 class Student < User
-  attr_reader :first_name, :last_name
-  
-  def initialize (first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
   end
 
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
